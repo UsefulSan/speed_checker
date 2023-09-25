f1e138f5b0aa14a592dcd737531a866a4e10cc0f
UsefulSan <usefulsan@yandex.ru>
2023-09-25 05:56:25 +0300

    Добавляет комментарии к методом

--- speed_checker/core.py
+++ speed_checker/core.py
@@ -15,6 +15,19 @@ cur = conn.cursor()
 
 class MyHttpRequestHandler(http.server.CGIHTTPRequestHandler):
     def do_GET(self) -> None:
+        """
+        Обрабатывает метод запроса GET.
+
+        Получает данные из таблицы construction_equipment в базе данных в соответствии с указанным путем и методом.
+        Если путь - "/resource", а метод - "GET", то извлекаются все строки таблицы и добавляется колонка "over_speed",
+        в которой показан % превышения максимальной скорости.
+        Если путь имеет вид "/resource/?type_equipment=<type_equipment>", а метод - "GET", то из таблицы извлекаются
+        строки, соответствующие шаблону type_equipment, и добавляется колонка "over_speed".
+        Наконец, она отправляет ответ с кодом состояния 200, устанавливает в заголовке "Content-type" значение
+        "application/json" и записывает в тело ответа полученные строки в виде JSON-строки.
+
+        Эта функция не принимает никаких параметров и ничего не возвращает.
+        """
         lst = self.requestline.split()
         method, path = lst[0], lst[1]
         if path == "/resource" and method == "GET":
@@ -34,6 +47,11 @@ class MyHttpRequestHandler(http.server.CGIHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps(rows), "utf8"))
 
     def do_POST(self) -> None:
+        """
+        Обработка POST-запроса.
+        Этот метод вызывается при поступлении на сервер POST-запроса.
+        Он считывает тело запроса, проверяет данные и вставляет их в таблицу 'construction_equipment' в базе данных.
+        """
         try:
             self.send_header("Content-type", "application/json")
             self.end_headers()
@@ -58,6 +76,13 @@ class MyHttpRequestHandler(http.server.CGIHTTPRequestHandler):
             self.send_error(400, "Bad request")
 
     def do_DELETE(self) -> None:
+        """
+        Отправляет на сервер запрос DELETE на удаление ресурса.
+
+        Эта функция отправляет на сервер запрос DELETE на удаление ресурса на основе предоставленного идентификатора
+        ресурса (идентификаторов). Она устанавливает для заголовка "Content-type" значение "application/json" и
+        завершает заголовки запроса.
+        """
         self.send_header("Content-type", "application/json")
         self.end_headers()
         resource_id_pattern = re.compile(r'^\/resource\/\?id=((\d+)?(,\d+)*)')
@@ -76,6 +101,14 @@ class MyHttpRequestHandler(http.server.CGIHTTPRequestHandler):
                 self.send_response(404, "Not found")
 
     def do_PUT(self) -> None:
+        """
+        Обрабатывает метод запроса PUT.
+
+        Эта функция отвечает за обработку метода запроса PUT. Она извлекает идентификатор ресурса из пути URL и
+        проверяет, существует ли ресурс в базе данных. Если ресурс существует, то на основе данных тела запроса
+        происходит обновление соответствующих полей. В зависимости от успеха или неудачи операции обновления функция
+        возвращает соответствующий код ответа.
+        """
         resource_id_pattern = re.compile(r'^\/resource\/\?id=((\d+)?(,\d+)*)')
         match = resource_id_pattern.match(self.path)
         if match:
